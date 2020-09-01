e2feadb2f57bdd77b7b302944689b838129773fa
alfin1998 <alfinyuki@gmail.com>
2020-09-01 15:38:07 +0700

    Average

---
+++ Grayscale/Grayscale/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grayscale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog bukaFile = new OpenFileDialog();
+            bukaFile.Filter = "Image File (*.bmp, *.jpg, *.jpeg)|*.bmp;*.jpg;*.jpeg";
+            if (DialogResult.OK == bukaFile.ShowDialog())
+            {
+                this.pictureBox1.Image = new Bitmap(bukaFile.FileName);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Tidak Ada citra yang akan diolah");
+            }
+            else
+            {
+                Bitmap b = new Bitmap((Bitmap)this.pictureBox1.Image);
+              
+                for (int i = 0; i < b.Width; i++)
+                {
+                    for (int j = 0; j < b.Height; j++)
+                    {
+                        Color c1 = b.GetPixel(i, j);
+                        int grayAvg = (c1.R + c1.G + c1.B) / 3;
+                        b.SetPixel(i, j, Color.FromArgb(grayAvg, grayAvg, grayAvg));
+                    }
+                  
+                }
+           
+                this.pictureBox2.Image = b;
+            }
+        }
+    }
+}
